8c6d41677962525a3651ff16af17b8507d937d64
fletchgraham <SGraham@MacBook-Pro.local>
2016-07-08 20:57:32 -0600

    Modelo

--- head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>fletchgraham</title>
-<meta name="viewport" content="initial-scale=1">
-<link href="style.css" rel="stylesheet" type="text/css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<script src="script.js"></script>
-</head>
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* canvas draw function. genetic material from an ancient website of mine */
 window.onload = function(){
     
 var color = "#6900ff";
@@ -43,27 +44,284 @@ NOTES ON THE DYNAMIC MENU
 */
     
 $(document).ready(function(){
+    
+
     //the 'hide' click
         $('.hide').click(function(){
         $('.show').show();
         $('.hide').hide ();
         $('#description').hide ();
-        $('.menu').animate({height:'72px'}, 300);});
+        $('.menu:not(#daario-naharis)').animate({height:'72px'}, 300);});
     
     //the 'show' click
         $('.show').click(function(){
         $('.hide').show();
         $('.show').hide();
         $('#description').show ();
-        $('.menu').animate({height:$('.menu')[0].scrollHeight + 15}, 300);});
+        $('.menu:not(#daario-naharis)').animate({height:$('.menu')[0].scrollHeight + 15}, 300);});
 
-   });
+    
+    /* these guys actually give the drag functionality to the cards and the menu */
+$('.grab').draggable();
+$('.menu').draggable();
+$('.daario-naharis').draggable();
+    
 
-$(document).ready(function() {
+/* this is for making the location of the floaty boxes random */
+    
+  var bodyWidth = window.innerWidth
+  var bodyHeight = window.innerHeight;
+    $('.grab').each(function(){
+    var grab_width = this.clientWidth;
+    var grab_height = this.clientHeight;
+  var randPosX = Math.floor((((Math.random()+.5)/2)*(bodyWidth - grab_width)));
+  var randPosY = Math.floor((((Math.random()+.5)/2)*(bodyHeight - grab_height)));
+  
+  $(this).css('left', randPosX);
+  $(this).css('top', randPosY);
+    });
+    
+/* this creates the little x in the top of each floaty box allowing the viewer to close them */
+    $( ".grab" ).append( "<div class='x'>X</div>" );
+    $( ".x" ).click(function() {
+        $(this).parent('div').css('display','none');
+    });
+
+    
+/* this makes the list decoration things change if the page is active */
     $("[href]").each(function() {
     if (this.href == window.location.href) {
         $(this).parent('li').addClass('active');
         }
     });
+
+/*
+
+these next functions are responsible for bringing the correct card to the top of the pile when its name is moused over in the menu.
+
+*/
+    
+$( "#100-days-project-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#100-days-project-card').css('z-index', topZ+1);
+  }
+);
+    
+$( "#nourishment-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#nourishment-card').css('z-index', topZ+1);
+  }
+);
+    
+$( "#anatomy-sculptures-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#anatomy-sculptures-card').css('z-index', topZ+1);
+  }
+);
+    
+$( "#website-sketches-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#website-sketches-card').css('z-index', topZ+1);
+  }
+);
+    
+$( "#transitions-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#transitions-card').css('z-index', topZ+1);
+  }
+);
+    
+$( "#binaries-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#binaries-card').css('z-index', topZ+1);
+  }
+);
+    
+$( "#sketches-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#sketches-card').css('z-index', topZ+1);
+  }
+);
+      
+$( "#bike-build-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#bike-build-card').css('z-index', topZ+1);
+  }
+);
+
+$( "#downloads-btn" ).mouseenter(
+  function() {
+        var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $('#downloads-card').css('z-index', topZ+1);
+  }
+);
+    
+
+
+/* this bit brings the clicked card to the front */
+    
+$('.grab').mousedown(function(){
+    var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $('.menu').each(function(){
+        var menuZ = parseInt($(this).css('z-index'), 10);
+        if (menuZ > topZ){
+            topZ = menuZ;
+        }
+    });
+    $(this).css('z-index', topZ+1);
 });
+
+
+$('.menu').mousedown(function(){
+    var topZ = 0;
+    $('.grab').each(function(){
+        var thisZ = parseInt($(this).css('z-index'), 10);
+        if (thisZ > topZ){
+            topZ = thisZ;
+        }
+    });
+    $(this).css('z-index', topZ+1);
+});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
